9c858f2f4ab55678aba01149b21d411f8d6d825a
BackCoder <skystom2@naver.com>
2022-07-30 18:48:03 +0900

    CodingTest_Programmers

--- src/main/java/Programmers/Lotto.java
+++ src/main/java/Programmers/Lotto.java
@@ -1,32 +1,45 @@
 package Programmers;
 
 import java.util.HashSet;
-
 public class Lotto {
     public static void main(String[] args) {
 
-
-        int[] lottos = {44, 1, 0, 0, 31, 25};
+        int[] lottos = {0, 0, 0, 0, 5, 17};
 
         int[] win_nums = {31, 10, 45, 1, 6, 19};
-
-
 
         HashSet<Integer> set1 = new HashSet();
         HashSet<Integer> set2 = new HashSet();
         for (int i = 0; i < 6; i++) {
             set1.add(lottos[i]);
-            set2.add(win_nums[i]);
+            set2.add(win_nums[i]); }
+
+        int zero = 0;
+        if (set1.contains(0)) {
+            zero = 6 - set1.size() + 1; }
+
+//        set2.removeAll(set1);
+
+        set2.retainAll(set1);
+
+        int b = set2.size();
+
+        System.out.println(b);
+
+        int max = 7;
+        int min = 7;
+
+        for (int i = 6; i >=0; i--) {
+            if ( b == i) {
+                max = max - i - zero;
+                min = min - i;
+                break;}
         }
 
-        int zero = 6 - set1.size();
+        if ( max>6){max = 6;}
+        if ( min>6){min = 6;}
 
-        set1.
-
-
-
-
-
-
+        int[] answer = {max, min};
+        System.out.println(answer[0] + " " +answer[1]);
     }
 }
---
+++ src/main/java/Programmers/ReportResult.java
@@ -0,0 +1,95 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class ReportResult {
+    public static void main(String[] args) {
+
+        String[] id_list = {"abb", "bb", "c", "d"};
+        String[] report = {"abb bb", "bb abb", "c bb", "bb abb", "d abb", "bb c", "d bb" };
+
+        HashSet<String> reportSet = new HashSet<>();
+
+
+        for (int a =0; a< report.length; a++){
+            reportSet.add(report[a]);
+        }
+
+        int k = 2;
+
+        int[] answer= new int[id_list.length];
+
+        ArrayList<String> attacked = new ArrayList<>();
+
+        for (String rep: reportSet
+             ) {
+            attacked.add(rep.split(" ")[1]);
+        }
+
+        //b d b d a
+        Collections.sort(attacked);
+// a b b d d
+
+        HashSet<String> ban  = new HashSet<>();
+
+        StringBuilder sb = new StringBuilder();
+        for (int j = 0; j < attacked.size(); j++) {
+            sb.append(attacked.get(j));
+        }
+// abbbbdddd
+
+        for (int i = 0; i < id_list.length; i++) {
+            StringBuilder count = new StringBuilder();
+
+            for (int j = 0; j < k; j++) {
+                count.append(id_list[i]);
+            }
+
+
+            // 11 222 3
+            // june juney tom lun
+            // june june juney juney juney tom
+            // junejune / juneyjuney /
+            // june june junejune junejune
+
+            if (String.valueOf(sb).contains(String.valueOf(count))) {
+                ban.add(id_list[i]);
+            }
+        }
+
+        // ban : b d
+        ArrayList<String> attacker = new ArrayList<>();
+
+
+        for (String a: reportSet
+             ) {
+
+            if(
+            ban.contains(a.split(" ")[1]))
+            {
+                attacker.add(a.split(" ")[0]);
+            }
+        }
+
+// attacker : 벤당한 사람을 신고한 사람 :  a c a  d a
+        // 5
+
+        for (int i = 0; i < id_list.length; i++) {
+            int emailcount = 0;
+            for (int j = 0; j < attacker.size(); j++) {
+                if (attacker.get(j).equalsIgnoreCase(id_list[i])){
+                    emailcount++;
+                }
+            }
+            answer[i] = emailcount;
+        }
+
+        for (int an :answer
+             ) {
+            System.out.println(an);
+        }
+
+    }
+}
